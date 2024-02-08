1c3ac5309f6650c8c4183ebca66bde54450681a2
rafaelreisramos <rrramos@gmail.com>
2024-02-08 14:44:48 -0300

    chore: add vote/scores on redis

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod'
 import { prisma } from '../../lib/prisma'
 import { FastifyInstance } from 'fastify'
+import { redis } from '../../lib/redis'
 
 export async function getPoll(app: FastifyInstance) {
   app.get('/polls/:pollId', async (request, reply) => {
@@ -21,6 +22,27 @@ export async function getPoll(app: FastifyInstance) {
         },
       },
     })
-    return reply.status(200).send({ poll })
+    if (!poll) return reply.status(400).send({ message: 'Poll not found.' })
+    const results = await redis.zrange(poll.id, 0, -1, 'WITHSCORES')
+    const votes = results.reduce((obj, line, index) => {
+      if (index % 2 === 0) {
+        const score = results[index + 1]
+        Object.assign(obj, { [line]: Number(score) })
+      }
+      return obj
+    }, {} as Record<string, number>)
+    return reply.status(200).send({
+      poll: {
+        id: poll.id,
+        title: poll.title,
+        options: poll.options.map((option) => {
+          return {
+            id: option.id,
+            title: option.title,
+            score: option.id in votes ? votes[option.id] : 0,
+          }
+        }),
+      },
+    })
   })
 }
--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,7 @@ import { randomUUID } from 'node:crypto'
 import { z } from 'zod'
 import { prisma } from '../../lib/prisma'
 import { FastifyInstance } from 'fastify'
+import { redis } from '../../lib/redis'
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/vote', async (request, reply) => {
@@ -41,6 +42,7 @@ export async function voteOnPoll(app: FastifyInstance) {
           id: userPreviousVoteOnPoll.id,
         },
       })
+      await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
     }
     await prisma.vote.create({
       data: {
@@ -49,6 +51,7 @@ export async function voteOnPoll(app: FastifyInstance) {
         pollOptionId,
       },
     })
+    await redis.zincrby(pollId, 1, pollOptionId)
     return reply.status(201)
   })
 }
